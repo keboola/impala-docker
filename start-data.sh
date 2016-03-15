8ca37b78f32879bc04c05bbf96604d86f5d99162
Miroslav Cillik <miro@keboola.com>
2016-03-15 17:05:40 +0100

    fix: load data

--- start-data.sh
+++ start-data.sh
@@ -4,9 +4,8 @@
 /start.sh
 
 # load test data
-mkdir -p /sample_data/escaping
-hdfs dfs -put escaping.csv /sample_data/escaping
-impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/sample_data/escaping'"
+su -c "hdfs dfs -put /escaping.csv /tmp/escaping.csv" impala
+impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/tmp'"
 
 # run as daemon
 while true
