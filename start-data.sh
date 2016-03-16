b3fb9b6b9a072f4bd46cbd2d07a592895304fa42
Miroslav Cillik <miro@keboola.com>
2016-03-16 11:52:57 +0100

    fix: sleep before loading data a bit

--- start-data.sh
+++ start-data.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# start
-/start.sh
+sleep 30s
 
 # load test data
 su -c "hdfs dfs -put /escaping.csv /tmp/escaping.csv" impala
 impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/tmp'"
 impala-shell -q "SELECT * FROM escaping"
 
-# run as daemonexit
+# run as daemon
 while true
 do
 	sleep 30
