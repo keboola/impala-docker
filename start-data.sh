10e92c11d1c63200f125910f43a62a9e56258be9
Miroslav Cillik <miro@keboola.com>
2016-03-16 11:30:15 +0100

    feat: test select * from escaping

--- start-data.sh
+++ start-data.sh
@@ -6,8 +6,9 @@
 # load test data
 su -c "hdfs dfs -put /escaping.csv /tmp/escaping.csv" impala
 impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/tmp'"
+impala-shell -q "SELECT * FROM escaping"
 
-# run as daemon
+# run as daemonexit
 while true
 do
 	sleep 30
