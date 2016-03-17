84633245b4e1df7890c2db2eb85f016dad9348b8
Miroslav Cillik <miro@keboola.com>
2016-03-17 15:48:58 +0100

    fix: sample data use varchar instead of string

--- start-data.sh
+++ start-data.sh
@@ -2,15 +2,11 @@
 
 /start.sh
 
-locale-gen "en_US.UTF-8"
-dpkg-reconfigure locales
-
 sleep 30s
 
 # load test data
 su -c "hdfs dfs -put /sales.csv /tmp/sales.csv" impala
-impala-shell -q "CREATE EXTERNAL TABLE sales (usergender string, usercity string, usersentiment int, zipcode string, sku string, createdat string, category string, price float, county string, countycode string, userstate string, categorygroup string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/tmp'"
-impala-shell -q "SELECT * FROM sales"
+impala-shell -q "CREATE EXTERNAL TABLE sales (usergender varchar(255), usercity varchar(255), usersentiment int, zipcode varchar(255), sku varchar(255), createdat varchar(255), category varchar(255), price float, county varchar(255), countycode varchar(255), userstate varchar(255), categorygroup varchar(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/tmp'"
 
 # run as daemon
 while true
