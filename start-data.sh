181902531f8bb32086e4892ea7c800bf7f80648f
Miroslav Cillik <miro@keboola.com>
2016-03-17 14:02:26 +0100

    fix: csv load data format

--- start-data.sh
+++ start-data.sh
@@ -6,7 +6,7 @@ sleep 30s
 
 # load test data
 su -c "hdfs dfs -put /escaping.csv /tmp/escaping.csv" impala
-impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' ESCAPED BY '' LOCATION '/tmp'"
+impala-shell -q "CREATE EXTERNAL TABLE escaping (col1 VARCHAR(255), col2 VARCHAR(255)) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' ESCAPED BY '%' LOCATION '/tmp'"
 impala-shell -q "SELECT * FROM escaping"
 
 # run as daemon
